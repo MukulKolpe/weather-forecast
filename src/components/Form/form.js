9c18c40ae0d1b76fef806eb99edda44406aa2cb5
Mukul <mukulkolpe45@gmail.com>
2021-09-24 18:57:36 +0530

    small updates

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -21,7 +21,7 @@ class Form extends Component{
 				</div>
 				<div className="row justify-content-center">
 						<button type="submit" className="btn btn-dark info mb-3">Get Weather <i className="fas fa-arrow-alt-circle-right ml-1"></i></button>
-					 {/* <Button type="submit" variant="contained">Get Weather</Button>	*/}
+					 {/* <Button type="submit" variant="contained">Get Weather<i className="fas fa-arrow-alt-circle-right ml-1"></i></Button>	*/}
 				</div>	
 					
 			</form>
